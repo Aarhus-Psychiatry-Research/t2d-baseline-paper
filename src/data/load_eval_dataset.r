03038545d16baee5d413261ff8e6c4685e133c3e
Martin Bernstorff <ryqiem@gmail.com>
2023-02-10 10:51:43 +0100

    feat: load baseline data to rmd

---
+++ src/data/load_eval_dataset.r
@@ -0,0 +1,10 @@
+library("pacman")
+p_load("arrow")
+
+load_eval_dataset <- function() {
+  path <- "E:/shared_resources/t2d/model_eval/chapelet-megaloblastic/rural-silence-2005/evaluation_dataset.parquet"
+  dataset <- read_parquet(path)
+  
+  
+  return(dataset)
+}
