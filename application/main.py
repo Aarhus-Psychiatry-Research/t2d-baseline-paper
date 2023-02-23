8625e0a3ea5b7e648716bb90b190bd11e6eb4595
Martin Bernstorff <ryqiem@gmail.com>
2023-02-23 09:22:57 +0100

    feat: add first scatter plot

--- application/main.py
+++ application/main.py
@@ -1,14 +1,37 @@
 from config import best_runs
-from figures.roc_curve import generate_roc_curve
-from figures.shap_values import beeswarm_pipeline
+from figures.shap_values import (
+    TrainSplitConf,
+    generate_shap_values,
+    get_train_split,
+    pipeline_loader,
+    plot_beeswarm,
+    plot_shap_scatter,
+)
 from zenml.pipelines import pipeline
 
 
 @pipeline(enable_cache=True)
-def output_pipeline():
-    
+def output_pipeline(
+    training_data_loader,
+    pipeline_loader,
+    shap_generator,
+    beeswarm_fn,
+    plot_shap_scatter,
+):
+    train_split = training_data_loader()
+    pipe = pipeline_loader()
+    shap_values = shap_generator(train_df=train_split, pipeline=pipe)
+    beeswarm_fn(shap_values=shap_values)
+    plot_shap_scatter(shap_values=shap_values, X=train_split)
+
 
 if __name__ == "__main__":
-    # generate_roc_curve(use_synth_data=False, best_runs=best_runs)
+    output_pipeline_instance = output_pipeline(
+        training_data_loader=get_train_split(TrainSplitConf(best_runs=best_runs)),
+        pipeline_loader=pipeline_loader(TrainSplitConf(best_runs=best_runs)),
+        shap_generator=generate_shap_values(),
+        beeswarm_fn=plot_beeswarm(),
+        plot_shap_scatter=plot_shap_scatter(),
+    )
 
-    shap_value_plot = beeswarm_pipeline(best_runs=best_runs).run()
+    output_pipeline_instance.run(unlisted=True)
