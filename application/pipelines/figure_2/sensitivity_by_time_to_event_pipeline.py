11bb157b462b084f089e29b06f4e21bc43a6c1c7
Martin Bernstorff <ryqiem@gmail.com>
2023-03-14 15:27:38 +0100

    feat: add bin delta

--- application/pipelines/figure_2/sensitivity_by_time_to_event_pipeline.py
+++ application/pipelines/figure_2/sensitivity_by_time_to_event_pipeline.py
@@ -13,9 +13,10 @@ def incidence_by_time_until_outcome_pipeline():
 
     plot_recall_by_calendar_time(
         eval_dataset=eval_ds,
-        month_bins=range(0, 37, 3),
+        bins=range(0, 37, 3),
         pos_rate=[0.05, 0.03, 0.01],
         y_limits=(0, 1),
+        bin_delta="M",
         save_path=FIGURES_PATH / "sensitivity_by_time_to_event.png",
     )
 
