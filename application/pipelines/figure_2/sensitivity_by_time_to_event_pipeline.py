4d912953028bb2e7495bbb8c008b16f37c17692c
Martin Bernstorff <ryqiem@gmail.com>
2023-03-14 15:33:29 +0100

    feat: modify y-axis

--- application/pipelines/figure_2/sensitivity_by_time_to_event_pipeline.py
+++ application/pipelines/figure_2/sensitivity_by_time_to_event_pipeline.py
@@ -15,7 +15,7 @@ def incidence_by_time_until_outcome_pipeline():
         eval_dataset=eval_ds,
         bins=range(0, 37, 3),
         pos_rate=[0.05, 0.03, 0.01],
-        y_limits=(0, 1),
+        y_limits=(0, 0.5),
         bin_delta="M",
         save_path=FIGURES_PATH / "sensitivity_by_time_to_event.png",
     )
