f41157b6575d618ee79f715762d6c0802ec3c591
Martin Bernstorff <ryqiem@gmail.com>
2023-03-15 14:30:03 +0100

    feat: more plots

---
+++ application/pipelines/robustness/robustness_by_time_from_first_visit.py
@@ -0,0 +1,24 @@
+from psycop_model_training.model_eval.base_artifacts.plots.performance_over_time import (
+    plot_auc_by_time_from_first_visit,
+)
+from t2d_baseline_paper.best_runs import ROBUSTNESS_PATH, best_run
+from t2d_baseline_paper.data.load_true_data import load_eval_dataset
+
+
+def roc_auc_by_calendar_time():
+    eval_ds = load_eval_dataset(
+        wandb_group=best_run.wandb_group,
+        wandb_run=best_run.model,
+        fraction=1,
+    )
+
+    plot_auc_by_time_from_first_visit(
+        eval_dataset=eval_ds,
+        bins=range(0, 37, 3),
+        bin_unit="M",
+        save_path=ROBUSTNESS_PATH / "auc_by_time_from_first_visit.png",
+    )
+
+
+if __name__ == "__main__":
+    roc_auc_by_calendar_time()
