bedb78d8f14b52e5195eeef11b5f6b4fef453fa4
Martin Bernstorff <ryqiem@gmail.com>
2023-03-15 13:41:52 +0100

    feat: add robustness by age

---
+++ application/pipelines/robustness/robustness_by_age.py
@@ -0,0 +1,23 @@
+from psycop_model_training.model_eval.base_artifacts.plots.performance_by_age import (
+    plot_performance_by_age,
+)
+from t2d_baseline_paper.best_runs import ROBUSTNESS_PATH, best_run
+from t2d_baseline_paper.data.load_true_data import load_eval_dataset
+
+
+def roc_auc_by_sex():
+    eval_ds = load_eval_dataset(
+        wandb_group=best_run.wandb_group,
+        wandb_run=best_run.model,
+        fraction=1,
+    )
+
+    plot_performance_by_age(
+        eval_dataset=eval_ds,
+        bins=[18, *range(20, 80, 5)],
+        save_path=ROBUSTNESS_PATH / "auc_by_age.png",
+    )
+
+
+if __name__ == "__main__":
+    roc_auc_by_sex()
