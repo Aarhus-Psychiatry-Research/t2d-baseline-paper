8e08a536df184aef7a4ade669e7ef05b25d1b93f
Martin Bernstorff <ryqiem@gmail.com>
2023-03-15 14:15:19 +0100

    feat: add robustness by calendar time

---
+++ application/pipelines/robustness/robustness_by_calendar_time.py
@@ -0,0 +1,27 @@
+from psycop_model_training.model_eval.base_artifacts.plots.performance_by_age import (
+    plot_performance_by_age,
+)
+from psycop_model_training.model_eval.base_artifacts.plots.performance_over_time import (
+    create_performance_by_calendar_time_df,
+    plot_metric_by_calendar_time,
+)
+from t2d_baseline_paper.best_runs import ROBUSTNESS_PATH, best_run
+from t2d_baseline_paper.data.load_true_data import load_eval_dataset
+
+
+def roc_auc_by_calendar_time():
+    eval_ds = load_eval_dataset(
+        wandb_group=best_run.wandb_group,
+        wandb_run=best_run.model,
+        fraction=1,
+    )
+
+    plot_metric_by_calendar_time(
+        eval_dataset=eval_ds,
+        bin_period="Q",
+        save_path=ROBUSTNESS_PATH / "auc_by_calendar_time.png",
+    )
+
+
+if __name__ == "__main__":
+    roc_auc_by_calendar_time()
