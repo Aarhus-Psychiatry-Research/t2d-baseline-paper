93f6e228320b94a40e52d366029a1aff21aaa5e7
Martin Bernstorff <martinbernstorff@gmail.com>
2023-04-27 09:22:55 +0200

    deps: typo in psycopmlutils

--- src/t2d/model_training/train_models_in_parallel.py
+++ src/t2d/model_training/train_models_in_parallel.py
@@ -2,6 +2,7 @@
 wandb.
 """
 
+from psycop_ml_utils.wandb.wandb_try_except_decorator import wandb_alert_on_exception
 from psycop_model_training.application_modules.get_search_space import (
     SearchSpaceInferrer,
 )
@@ -9,7 +10,6 @@ from psycop_model_training.application_modules.process_manager_setup import setu
 from psycop_model_training.application_modules.trainer_spawner import spawn_trainers
 from psycop_model_training.config_schemas.full_config import FullConfigSchema
 from psycop_model_training.data_loader.data_loader import DataLoader
-from psycopmlutils.wandb.wandb_try_except_decorator import wandb_alert_on_exception
 
 
 @wandb_alert_on_exception
