1aff8e3725f382bddc8bb12eeac8d01e190fe145
Martin Bernstorff <ryqiem@gmail.com>
2023-05-05 13:31:43 +0200

    feat: add SHAP table to outputs

--- src/t2d/paper_outputs/model_description/feature_importance/shap/main.py
+++ src/t2d/paper_outputs/model_description/feature_importance/shap/main.py
@@ -8,11 +8,22 @@ from t2d.paper_outputs.model_description.feature_importance.shap.get_shap_values
 )
 
 long_shap_df = get_shap_bundle_for_best_run(
-    n_rows=100_000, cache_ver=0.01
+    n_rows=1_000_000, cache_ver=0.01
 ).get_long_shap_df()
 
 # %%
 import polars as pl
+from t2d.paper_outputs.model_description.feature_importance.shap.shap_table import (
+    get_top_i_shap_values_for_printing,
+)
+
+table_df = get_top_i_shap_values_for_printing(
+    shap_long_df=pl.from_pandas(long_shap_df), i=100
+)
+table_df
+
+
+# %%
 from t2d.paper_outputs.config import FIGURES_PATH, OUTPUT_MAPPING
 
 shap_figures_path = FIGURES_PATH / OUTPUT_MAPPING.shap_plots
