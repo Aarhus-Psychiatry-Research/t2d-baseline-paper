cd25fd1f1fbaa99ec13da7e057a66ebf5cefc535
Martin Bernstorff <ryqiem@gmail.com>
2023-05-05 13:32:09 +0200

    feat: use mean absolute shap instead of shap variance

--- src/t2d/paper_outputs/model_description/feature_importance/shap/shap_table.py
+++ src/t2d/paper_outputs/model_description/feature_importance/shap/shap_table.py
@@ -7,16 +7,16 @@ def get_top_i_shap_values_for_printing(
 ) -> pl.DataFrame:
     aggregated = shap_long_df.groupby("feature_name").agg(
         pl.col("feature_name").first().alias("Feature"),
-        pl.col("shap_value").std().alias("SHAP variance"),
+        pl.col("shap_value").abs().mean().alias("Mean absolute SHAP"),
     )
 
-    ranked = aggregated.sort(by="SHAP variance", descending=True).select(
-        pl.col("SHAP variance")
+    ranked = aggregated.sort(by="Mean absolute SHAP", descending=True).select(
+        pl.col("Mean absolute SHAP")
         .rank(method="average", descending=True)
         .cast(pl.Int64)
         .alias("Rank"),
         pl.col("Feature"),
-        pl.col("SHAP variance").round(2).alias("SHAP variance"),
+        pl.col("Mean absolute SHAP").round(2).alias("Mean absolute SHAP"),
     )
 
     return ranked.head(i)
