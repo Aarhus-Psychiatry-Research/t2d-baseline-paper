a46fd93bf9b041908e7642c980f5f9a9da9c13d1
Martin Bernstorff <martinbernstorff@gmail.com>
2023-05-05 13:54:54 +0200

    ci: swap black and ruff for pre-commit

--- src/t2d/paper_outputs/model_description/feature_importance/shap/main.py
+++ src/t2d/paper_outputs/model_description/feature_importance/shap/main.py
@@ -8,7 +8,8 @@ from t2d.paper_outputs.model_description.feature_importance.shap.get_shap_values
 )
 
 long_shap_df = get_shap_bundle_for_best_run(
-    n_rows=1_000_000, cache_ver=0.01,
+    n_rows=1_000_000,
+    cache_ver=0.01,
 ).get_long_shap_df()
 
 # %%
@@ -18,7 +19,8 @@ from t2d.paper_outputs.model_description.feature_importance.shap.shap_table impo
 )
 
 table_df = get_top_i_shap_values_for_printing(
-    shap_long_df=pl.from_pandas(long_shap_df), i=100,
+    shap_long_df=pl.from_pandas(long_shap_df),
+    i=100,
 )
 table_df
 
@@ -34,7 +36,9 @@ from t2d.paper_outputs.model_description.feature_importance.shap.plot_shap impor
 )
 
 save_plots_for_top_i_shap_by_variance(
-    shap_long_df=pl.from_pandas(long_shap_df), i=4, save_dir=shap_figures_path,
+    shap_long_df=pl.from_pandas(long_shap_df),
+    i=4,
+    save_dir=shap_figures_path,
 )
 
 # %%
